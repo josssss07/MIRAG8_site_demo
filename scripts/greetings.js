bce885256c1061d318f4c6cd3d4c5846d7d7103e
josssss07 <joshuadesai03@gmail.com>
2024-04-14 12:30:33 +0530

    news basic built and style changes

--- scripts/greetings.js
+++ scripts/greetings.js
@@ -2,19 +2,42 @@ let currentDate = new Date();
 let hours = currentDate.getHours().toString().padStart(2, '0');
 let minutes = currentDate.getMinutes().toString().padStart(2, '0');
 let formattedTime = `${hours}:${minutes}`;
-console.log("formatted time: "+formattedTime);
 const currentTime = new Date().getHours();
 let greeting;
 let currentTimeIndication = "The current time is : "+ formattedTime;
 
 if(currentTime<12){
-    greeting = "Good morning user";
+    let morning_greetings = [
+        "Good morning!",
+        "Have a great start to your day",
+        "Glad to see you today",
+        "Top of the morning!",
+        "Rise and Shine",
+        "I wish you a productive morning",
+    ];
+    greeting = morning_greetings[Math.floor(Math.random() * 6)];
 }
 else if(currentTime<17){
-    greeting = "Good afternoon user";
+    let afternoons_greets = [
+        'Good Afternoon!',
+        "Hey There!",
+        "Wishing you a delightful afternoon",
+        'Hope you day is going as smoothly as possible',
+        'Sending you warm afternoon regards',
+        'Have a wonderful day ahead'
+    ];  
+    greeting = afternoons_greets[Math.floor(Math.random() * 6)];
 }
 else{
-    greeting = "Good evening user"
+    let morning_greetings = [
+        "Good Evening",
+        "Enjoy your evening",
+        "Hope you had a great day",
+        "Make the most of your evening",
+        "Enjou your night",
+        "Sleep well for a great day ",
+    ];
+    greeting = morning_greetings[Math.floor(Math.random() * 6)];
 }
 document.getElementById('greetings').textContent = greeting
 
