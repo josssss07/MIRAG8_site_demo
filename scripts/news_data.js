8ce9b0278e297b20b917e0f6fe6466c6c7775f4c
josssss07 <joshuadesai03@gmail.com>
2024-05-09 17:15:22 +0530

    news api and basic stuff added

--- scripts/news_data.js
+++ scripts/news_data.js
@@ -1,12 +1,27 @@
-// fetch(' https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=&pageSize=1')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Process the fetched data
-//     console.log(data);
-//     let newsHeadLine = document.getElementById();
-//     let news
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
+window.onload = function() {
 
+    const apiUrl = 'https://newsapi.org/v2/top-headlines';
+    const apiKey = '1a8d9c287b604937b38c475562e315d0';
+  
+    const titleElement = document.getElementById('todays-events-title');
+    const infoElement = document.getElementById('todays-events-info');
+  
+    if (titleElement && infoElement) {
+  
+      fetch(`${apiUrl}?country=us&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+  
+          const latestArticle = data.articles[0];
+          titleElement.textContent = latestArticle.title;
+          infoElement.textContent = latestArticle.description;
+        })
+        .catch(error => {
+          console.error('Error fetching news:', error);
+          // Handle the error here, like displaying an error message to the user
+        });
+    } else {
+      console.error('HTML elements not found');
+    }
+  };
+  
