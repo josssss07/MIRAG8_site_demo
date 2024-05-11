67cb24e15a259660fd8bd5ab2befddf9d6fb53c3
AdleenaLobo <1069455+AdleenaLobo@users.noreply.github.com>
2024-05-11 11:52:14 +0530

    add news api

--- scripts/news_data.js
+++ scripts/news_data.js
@@ -1,27 +1,32 @@
-window.onload = function() {
-
-    const apiUrl = 'https://newsapi.org/v2/top-headlines';
-    const apiKey = '1a8d9c287b604937b38c475562e315d0';
+function callNews(){
+  const api = 'https://gnews.io/api/v4/top-headlines?category=general&apikey=';
+    const apiKey = 'da2b1b1325969d4515a72292f185bdb7';
   
     const titleElement = document.getElementById('todays-events-title');
     const infoElement = document.getElementById('todays-events-info');
-  
     if (titleElement && infoElement) {
   
-      fetch(`${apiUrl}?country=us&apiKey=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-  
-          const latestArticle = data.articles[0];
-          titleElement.textContent = latestArticle.title;
-          infoElement.textContent = latestArticle.description;
-        })
-        .catch(error => {
+      fetch(api+apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log({data});
+        const latestArticle = data.articles[0];
+        titleElement.textContent = latestArticle.title;
+        infoElement.textContent = latestArticle.description;
+
+    }).catch(error => {
           console.error('Error fetching news:', error);
-          // Handle the error here, like displaying an error message to the user
-        });
-    } else {
+           // Handle the error here, like displaying an error message to the user
+         });
+     } else {
       console.error('HTML elements not found');
+       
+
     }
-  };
+  }
+
+    callNews();
+
   
